57c8372da94a2dd8314afcd44b28e6775f6d4fca
Stalin Pereira <stalin@imatefx.com>
2025-12-17 13:15:32 +0530

    corrected path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// REPLACE 'repo-name' WITH THE NAME OF YOUR GITHUB REPOSITORY
 export default defineConfig({
   plugins: [react()],
-  base: '/control-tower/', 
+  base: '/', 
 })
